239a578e369964513e74f9072fb98d059032aa4c
Phattarachai Chaimongkol <phattarachai@entercorp.net>
2017-09-26 13:26:56 +0700

    Add SOLID workshop classes

---
+++ app/Workshop/SOLID/Order.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Workshop\SOLID;
+
+class Order {
+
+    /** @string */
+    public $id;
+
+    /** @double */
+    public $price;
+
+    /** @string */
+    public $category;
+
+    /**
+     * @return mixed
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrice() {
+        return $this->price;
+    }
+
+    /**
+     * @param mixed $price
+     */
+    public function setPrice($price) {
+        $this->price = $price;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCategory() {
+        return $this->category;
+    }
+
+    /**
+     * @param mixed $category
+     */
+    public function setCategory($category) {
+        $this->category = $category;
+    }
+
+}
---
+++ app/Workshop/SOLID/OrderProcessor.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Workshop\SOLID;
+
+class OrderProcessor {
+
+    public function process(Order $order, string $userID) {
+        $this->placeOrder($order, $userID);
+        $this->logOrder($order, $userID);
+    }
+
+    protected function placeOrder(Order $order, string $userID) {
+        //place order in DB
+        //Connect DB
+        //Write DB
+        //..... etc.
+    }
+
+    protected function logOrder(Order $order, string $userID) {
+        //Log order in DB
+        //Connect DB
+        //Write DB
+        //..... etc.
+
+        //throw new Exception("error");
+    }
+
+}
